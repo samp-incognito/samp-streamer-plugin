5a92507ed96002b70ebbda954fc09409eb533c10
samp-incognito <someone.extraordinary@gmail.com>
2016-05-28 21:43:48 -0400

    Update version number

--- src/main.h
+++ src/main.h
@@ -17,7 +17,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define INCLUDE_FILE_VERSION (0x281001)
-#define PLUGIN_VERSION "2.8.1"
+#define INCLUDE_FILE_VERSION (0x282001)
+#define PLUGIN_VERSION "2.8.2"
 
 #endif
