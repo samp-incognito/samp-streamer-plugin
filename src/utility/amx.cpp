b6a2491aa13f1a7f81cf6830f6119c609cd317ad
samp-incognito <someone.extraordinary@gmail.com>
2019-01-16 20:49:16 -0600

    Update Travis CI links and fix another compile error

--- src/utility/amx.cpp
+++ src/utility/amx.cpp
@@ -61,7 +61,7 @@ int Utility::checkInterfaceAndRegisterNatives(AMX *amx, AMX_NATIVE_INFO *amxNati
 				includeFileVersion = static_cast<int>(*amxPhysAddr);
 			}
 		}
-		std::ostringstream includeFileVersionStream;
+		std::ostringstream includeFileVersionStream, pluginVersionStream;
 		if (includeFileVersion <= 0)
 		{
 			includeFileVersionStream << "unknown version";
@@ -69,15 +69,16 @@ int Utility::checkInterfaceAndRegisterNatives(AMX *amx, AMX_NATIVE_INFO *amxNati
 		else
 		{
 			includeFileVersionStream << std::hex << std::showbase << includeFileVersion;
-			std::istringstream(includeFileVersionStream.str().substr(0, std::to_string(INCLUDE_FILE_VERSION).length() + 2)) >> std::hex >> includeFileVersion;
+			pluginVersionStream << std::hex << std::showbase << INCLUDE_FILE_VERSION;
+			std::istringstream(includeFileVersionStream.str().substr(0, pluginVersionStream.str().length())) >> std::hex >> includeFileVersion;
 		}
 		if (includeFileVersion < INCLUDE_FILE_VERSION)
 		{
-			Utility::logError("The include file version (%s) for this script is older than the plugin version (%#x). The script might need to be recompiled with the latest include file.", includeFileVersionStream.str().c_str(), INCLUDE_FILE_VERSION);
+			Utility::logError("The include file version (%s) for this script is older than the plugin version (%#x). The script might need to be recompiled with latest include file.", includeFileVersionStream.str().c_str(), INCLUDE_FILE_VERSION);
 		}
 		else if (includeFileVersion > INCLUDE_FILE_VERSION)
 		{
-			Utility::logError("The plugin version (%#x) is older than the include file version (%s) for this script. The plugin might need to be updated to the latest version.", INCLUDE_FILE_VERSION, includeFileVersionStream.str().c_str());
+			Utility::logError("The plugin version (%#x) is older than the include file version (%s) for this script. The plugin might need to be updated to latest version.", INCLUDE_FILE_VERSION, includeFileVersionStream.str().c_str());
 		}
 	}
 	if (hookedNatives)
